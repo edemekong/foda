85c99acf4151df57317d2f246bd0208cd082310d
Paul Edemekong <hello.paulvin@gmail.com>
2022-04-09 09:06:32 +0100

    setup app theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,34 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:foda/themes/app_theme.dart';
+
+void main() {
+  runApp(const FodaApp());
+}
+
+class FodaApp extends StatelessWidget {
+  const FodaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.theme,
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Foda',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+    );
+  }
+}
